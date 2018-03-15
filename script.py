255bf40f59042f903881652489e1036b5746a88f
Fernando Mendez Requena <fernando.mendez@atos.net>
2018-03-15 12:03:00 +0100

    It supports sending the requests using a delay time

--- script.py
+++ script.py
@@ -32,18 +32,15 @@ import os
 from urllib2 import Request, urlopen
 import urlparse
 from datetime import datetime
+import time
 
 
-#Addres path
 CONFIG_FILE =  './config/config.json'
-
-
 ENTITY_NAME = 'MillingMachine005'
 ENTITY_TYPE = 'Machine'
 ENTITY_CATEGORY = 'millingMachine'
 
 NUM_ARG=len(sys.argv)
-
 SCRIPT_NAME=sys.argv[0]
 FILE_NAME_CSV= sys.argv[1]
 
@@ -51,8 +48,6 @@ if NUM_ARG !=2:
 	print 'Usage: ' + SCRIPT_NAME + '[CSV NAME]'
  
 CSV = FILE_NAME_CSV
-
-
 
 headers = {
   'Content-Type': 'application/json'
@@ -64,13 +59,11 @@ else:
 	URL = "http://localhost:1026"
 
 
-
 def readConfigFile():
 	config = []
 	data = json.load(open(CONFIG_FILE))
 		
 	for i in data["config"]:
-		#dic.update({ i["colunm_name"].encode("ascii","replace"): i["type"].encode("ascii","replace")})
 		config.append({"colunm_name": i["colunm_name"].encode("ascii","replace"),
 			"type": i["type"].encode("ascii","replace"),
 			"attribute_name": i["attribute_name"].encode("ascii","replace")});
@@ -78,11 +71,8 @@ def readConfigFile():
 
 
 def attributeNameType():
-	
-	data = json.load(open(CONFIG_FILE))
-	
+	data = json.load(open(CONFIG_FILE))	
 	lista = []
-
 	for i in data["config"]:
 		lista.append({"name": i["attribute_name"].encode("ascii","replace"), "type": i["type"].encode("ascii","replace")})
 		
@@ -99,6 +89,16 @@ def casting(value, typeData):
 		
 	return castingValue
 
+def totalRowsCSV():
+	my_file = os.path.join('./', CSV)
+	
+	with open(my_file) as csvfile:
+		reader = csv.DictReader(csvfile, delimiter=';', quoting=csv.QUOTE_NONE)
+		attributeName = reader.fieldnames		
+		rows = list(reader)
+		totalrows= len(rows)
+	return totalrows
+
 def sendMeasures():
 	my_file = os.path.join('./', CSV)
 	configTranslate = readConfigFile()
@@ -111,23 +111,27 @@ def sendMeasures():
 	val = "value"
 	content = {}
 	i = 0
+	seconds_viejos= 0.00
+	delay = 0.00
+	seconds1 = float(0.00)
+
 	with open(my_file) as csvfile:
 		reader = csv.DictReader(csvfile, delimiter=';', quoting=csv.QUOTE_NONE)
-		attributeName = reader.fieldnames
+		attributeName = reader.fieldnames		
+		total = totalRowsCSV()
+			
 		for row in reader:
+			i = i +1
 			date = row[attributeName[0]]
-			time = row[attributeName[1]]
+			times = row[attributeName[1]]
 
 			d = datetime.strptime(date,'%d-%b-%Y')
 			newDate = d.strftime('%Y-%m-%d')
-			
+			hour= int(times.split(':')[0])
+			minute=int(times.split(':')[1])
+			seconds=float(times.split(':')[2])
 
-			hour= int(time.split(':')[0])
-			minute=int(time.split(':')[1])
-			seconds=float(time.split(':')[2])
-
-			
-			string_date = str(newDate+time)
+			string_date = str(newDate+times)
 			aux = str(datetime.strptime(string_date, "%Y-%m-%d%H:%M:%S.%f"))
 
 			#import pyt
@@ -141,10 +145,9 @@ def sendMeasures():
 			new_month = str(new_date.split("-")[1])
 			new_day = str(new_date.split("-")[2])
 			aux_new_date=new_year+"-"+new_month+"-"+new_day+"T"
-			
-	
+		
 			new_time=str(aux.split(" ")[1])
-
+		
 			new_hours = str(new_time.split(":")[0])
 			new_minutes=str(new_time.split(":")[1])
 			new_seconds=str(new_time.split(":")[2])		
@@ -152,42 +155,55 @@ def sendMeasures():
 
 			timestamp = str(aux_new_date+aux_new_time)
 
-			
 			# import dateutil.parser
-
 			# aux =  dateutil.parser.parser(aux)
-
 			# print datetime.
 
 			content["TimeInstant"] = {ty: "ISO8601", val: timestamp}
 			content["currentPart"] = {ty: "text", val: CSV}
 
 			for j in configTranslate:
-
 				value = row[j.get("colunm_name")]
 				types = j.get("type")
                         	cast = casting(value, types)
-
 				if value !="":
 					content[j.get("attribute_name")]  = { ty: types, val: cast} 
 			
 			output = json.dumps(content )
+			
+			t = seconds - seconds_viejos
+
+			seconds_viejos = float(seconds)
+			minutes_viejos = float(minute)
+
+			# if seconds_viejos <= 0:
+			# 	seconds_viejos * -1
+
+			if t <= 0:
+				t = (minute * 60) + seconds - (minutes_viejos * 60) + seconds_viejos						
+			
+			#print "minutes + seconds parte negativa: ", t
+			#print "delay", t
+		
+			print "Sending measure number: ", i
+			print "Time delay used: "+str(t)+" seconds"
+
+			time.sleep(t)
+
 			r = requests.post(final,data=output, headers=headers)
 				
-		        print str(r.status_code)
-			print str(r.text)
-			i=0
+		        print "Response: " + str(r.status_code)
+			#print str(r.text)
+			print "--------------------------------------"
+			print 
+
+			if i == total:
+				print "Finished the manufacture of the piece: ", CSV
 			
-
-
 						
 def createEntity():
-	content = { 'type': ENTITY_TYPE, 'id': ENTITY_NAME} #, 'category': ENTITY_CATEGORY}
+	content = { 'type': ENTITY_TYPE, 'id': ENTITY_NAME }
 	
-	#content["type"] = ENTITY_TYPE 
-	#content["id"]= ENTITY_NAME				
-	#content["category"] = "ENTITY_CATEGORY"	
-
 	my_file = os.path.join('./', CSV)
 	configTranslate = readConfigFile()
 	ty = "type"
@@ -200,17 +216,14 @@ def createEntity():
 			value = 0
 			types = j.get("type")
                        	cast = casting(value, types)
-
 			attributeName = j.get("attribute_name")
 			content[attributeName] = { ty: types, val: value}
-		
 		output = json.dumps(content,  encoding='ascii', ensure_ascii=True, indent=4)
 	
 	return output
 			
 
 if __name__ == "__main__":
-
 	payloadEntity = createEntity()
 	
 	parts = urlparse.urlparse(URL)
@@ -220,15 +233,8 @@ if __name__ == "__main__":
 	r = requests.post(new_url, data = payloadEntity, headers=headers)
 	if ((r.status_code == 201) or (r.status_code == 422)):
 	 	print "Initial entity created"
-		
+	 	print 
 		sendMeasures()
 	else:
 		print "unexpected errror"
 		print r.text
-
-
-	
-
-
-
-	
